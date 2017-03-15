5c56145b99a82efd41ad18845c83393d34748de9
danPudwellOpencast <dan.pudwell@opencastsoftware.com>
2017-03-15 17:10:06 +0000

    updated dependency versions

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -9,12 +9,12 @@ private object AppDependencies {
   import play.sbt.PlayImport._
   import play.core.PlayVersion
 
-  private val playHealthVersion = "2.0.0"
-  private val microserviceBootstrapVersion = "5.8.0"
-  private val playConfigVersion = "3.0.0"
-  private val playAuthorisationVersion = "4.2.0"
+  private val playHealthVersion = "2.1.0"
+  private val microserviceBootstrapVersion = "5.13.0"
+  private val playConfigVersion = "4.3.0"
+  private val playAuthorisationVersion = "4.3.0"
   private val playJsonLoggerVersion = "3.1.0"
-  private val domainVersion = "4.0.0"
+  private val domainVersion = "4.1.0"
   private val hmrcTestVersion = "2.1.0"
   private val scalaTestVersion = "2.2.6"
   private val scalaTestPlusVersion = "1.5.1"
