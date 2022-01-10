cf05db1d347012e740bf52a4ceae30739e37234b
Paul Anderson <15211314+paulanderson1234@users.noreply.github.com>
2022-01-10 15:45:13 +0000

    DL-7046 dependency and plugin updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ private object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-backend-play-28" % "5.16.0",
+    "uk.gov.hmrc" %% "bootstrap-backend-play-28" % "5.18.0",
     "uk.gov.hmrc" %% "domain"                    % "6.2.0-play-28"
   )
 
@@ -21,10 +21,10 @@ private object AppDependencies {
         "org.scalatestplus.play"       %% "scalatestplus-play"     % "5.1.0"             % scope,
         "org.pegdown"                  %  "pegdown"                % "1.6.0"             % scope,
         "com.typesafe.play"            %% "play-test"              % PlayVersion.current % scope,
-        "org.mockito"                  %  "mockito-core"           % "4.0.0"             % scope,
-        "com.github.tomakehurst"       %  "wiremock-jre8"          % "2.31.0"            % scope,
-        "com.fasterxml.jackson.module" %% "jackson-module-scala"   % "2.13.0"            % scope,
-        "uk.gov.hmrc"                  %% "bootstrap-test-play-28" % "5.16.0"            % scope
+        "org.mockito"                  %  "mockito-core"           % "4.2.0"             % scope,
+        "com.github.tomakehurst"       %  "wiremock-jre8"          % "2.32.0"            % scope,
+        "com.fasterxml.jackson.module" %% "jackson-module-scala"   % "2.13.1"            % scope,
+        "uk.gov.hmrc"                  %% "bootstrap-test-play-28" % "5.18.0"            % scope
       )
     }.test
   }
